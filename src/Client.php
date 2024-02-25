d66fb3d6c0ac4196daa25c8c72f47bcd98b4cf52
Artem Vasilev <kern.usr@gmial.com>
2024-02-25 19:08:12 +0300

    Добавил doc block по всем доступным методам API

--- src/Client.php
+++ src/Client.php
@@ -10,20 +10,70 @@ use Webmasterskaya\Unisender\Exception\InvalidArgumentException;
 use Webmasterskaya\Unisender\Exception\UnisenderException;
 
 /**
+ * Клиент для работы с рассылками
+ *
+ * Работа со списками контактов
+ * @method array getLists(array $data) Метод для получения списков рассылок с их кодами.
  * @method array createList(array $data) Метод для создания нового списка контактов.
- * @method array deleteList(array $data) Метод для удаления списка.
- * @method array exclude(array $data) Метод исключает e-mail или телефон контакта из одного или нескольких списков. В отличие от метода unsubscribe, он не помечает контакт как "отписавшийся", и его позднее снова можно включить в список с помощью метода subscribe.
- * @method array exportContacts(array $data) Экспорт данных контактов из Unisender.
- * @method array getContactCount(array $data) Метод для получения количества контактов в списке. Для данного метода существует лимит — 60 запросов/60 секунд.
- * @method array getLists() Метод для получения перечня всех имеющихся списков рассылок.
- * @method array getTotalContactsCount() Метод возвращает размер базы контактов по логину пользователя.
- * @method array importContacts(array $data) Метод массового импорта контактов. Может использоваться также для периодической синхронизации с базой контактов
- * @method array subscribe(array $data) Метод добавляет контакты (email адрес и/или мобильный телефон) контакта в один или несколько списков, а также позволяет добавить/поменять значения дополнительных полей и меток.
- * @method array unsubscribe(array $data) Метод отписывает e-mail или телефон контакта от одного или нескольких списков. Помечает контакт как «отписавшийся». Вернуть статус на «активный» через API нельзя!
  * @method array updateList(array $data) Метод для изменения свойств списка рассылки.
- * @method array getContact(array $data) Получение информации об одном контакте.
- * @method array isContactInLists(array $data) Метод используется для проверки, находится ли контакт в указанном(ых) списках пользователя.
- * @method array sendEmail(array $data) Метод для отправки одного индивидуального email-сообщения без использования персонализации и с ограниченными возможностями получения статистики.
+ * @method array deleteList(array $data) Метод для удаления списка контактов.
+ * @method array subscribe(array $data) Метод для подписки адресата на один или несколько списков рассылки.
+ * @method array exclude(array $data) Метод для исключения адресата из списков рассылки.
+ * @method array unsubscribe(array $data) Метод для отписывания адресата от рассылки.
+ * @method array importContacts(array $data) Метод для массового импорта и синхронизации контактов.
+ * @method array exportContacts(array $data) Метод для экспорта данных по контактам.
+ * @method array getTotalContactsCount(array $data) Метод для получения информации о размере базы пользователя.
+ * @method array getContactCount(array $data) Метод для получения количества контактов в списке.
+ * @method array getContact(array $data) Метод для получения информации об одном контакте.
+ *
+ * Работа с дополнительными полями и метками
+ * @method array getFields(array $data) Метод для получения списка пользовательских полей.
+ * @method array createField(array $data) Метод для создания нового пользовательского поля.
+ * @method array updateField(array $data) Метод для изменения параметров пользовательского поля.
+ * @method array deleteField(array $data) Метод для удаления пользовательского поля.
+ * @method array getTags(array $data) Метод для получения списка пользовательских меток.
+ * @method array deleteTag(array $data) Метод для удаления пользовательской метки.
+ *
+ * Создание и отправка сообщений
+ * @method array createEmailMessage(array $data) Метод для создания email для рассылки.
+ * @method array createSmsMessage(array $data) Метод для создания SMS для массовой рассылки.
+ * @method array createCampaign(array $data) Метод для планирования массовой отправки email или SMS сообщения.
+ * @method array cancelCampaign(array $data) Метод для отмены запланированной массовой рассылки.
+ * @method array getActualMessageVersion(array $data) Метод для получения актуальной версии письма.
+ * @method array sendSms(array $data) Метод для отправки SMS-сообщения.
+ * @method array checkSms(array $data) Метод для проверки статуса доставки SMS.
+ * @method array sendEmail(array $data) Метод для упрощенной отправки индивидуальных email-сообщений.
+ * @method array sendTestEmail(array $data) Метод для отправки тестовой email-рассылки на собственный адрес.
+ * @method array checkEmail(array $data) Метод для проверки статуса доставки email.
+ * @method array updateOptInEmail(array $data) Метод для изменения текста письма со ссылкой подтверждения подписки.
+ * @method array getWebVersion(array $data) Метод для получения ссылки на веб-версию отправленного письма.
+ * @method array deleteMessage(array $data) Метод для удаления сообщения.
+ * @method array updateEmailMessage(array $data) Метод для редактирования email для массовой рассылки.
+ *
+ * Работа с шаблонами
+ * @method array createEmailTemplate(array $data) Метод для создания шаблона сообщения для массовой рассылки.
+ * @method array updateEmailTemplate(array $data) Метод для редактирования существующего шаблона сообщения.
+ * @method array deleteTemplate(array $data) Метод для удаления шаблона.
+ * @method array getTemplate(array $data) Метод для получения информации о шаблоне.
+ * @method array getTemplates(array $data) Метод для получения списка всех шаблонов, созданных в системе.
+ * @method array listTemplates(array $data) Метод для получения списка всех шаблонов без body.
+ *
+ * Получение статистики
+ * @method array getCampaignDeliveryStats(array $data) Метод для получения отчета о статусах доставки сообщений для заданной рассылки.
+ * @method array getCampaignCommonStats(array $data) Метод для получения общих сведений о результатах доставки для заданной рассылки.
+ * @method array getVisitedLinks(array $data) Метод для получения статистики переходов по ссылкам.
+ * @method array getCampaigns(array $data) Метод для получения списка рассылок.
+ * @method array getCampaignStatus(array $data) Метод для получения статуса рассылки.
+ * @method array getMessages(array $data) Метод для получения списка сообщений.
+ * @method array getMessage(array $data) Метод для получения информации об SMS или email-сообщении.
+ * @method array listMessages(array $data) Метод для получения списка сообщений без тела и вложений.
+ *
+ * Работа с заметками
+ * @method array createSubscriberNote(array $data) Метод для создания заметки о контакте.
+ * @method array updateSubscriberNote(array $data) Метод для редактирования заметки.
+ * @method array deleteSubscriberNote(array $data) Метод для удаления заметки.
+ * @method array getSubscriberNote(array $data) Метод для получения информации о заметке.
+ * @method array getSubscriberNotes(array $data) Метод для получения информации о всех заметках контакта.
  */
 class Client
 {
@@ -161,7 +211,7 @@ class Client
     protected function execute(string $method, array $data = [])
     {
         /**
-         * @var \Psr\Http\Client\ClientInterface          $client
+         * @var \Psr\Http\Client\ClientInterface $client
          */
         static $client;
 
@@ -179,7 +229,7 @@ class Client
 
         // TODO: Валидация аргументов
         $data['api_key'] = $this->api_key;
-        $data['format']  = 'json';
+        $data['format'] = 'json';
 
         $uri = sprintf('https://api.unisender.com/%s/api/%s',
             $this->language,
@@ -209,11 +259,11 @@ class Client
 
         $status = $response->getStatusCode();
         if ($status >= 400 && $status < 500) {
-            throw new UnisenderException('HTTP Client error: ' . $response->getReasonPhrase() . '. Text: ' .$response->getBody());
+            throw new UnisenderException('HTTP Client error: ' . $response->getReasonPhrase() . '. Text: ' . $response->getBody());
         }
 
         if ($status >= 500) {
-            throw new UnisenderException('HTTP Server error: ' . $response->getReasonPhrase() . '. Text: ' .$response->getBody());
+            throw new UnisenderException('HTTP Server error: ' . $response->getReasonPhrase() . '. Text: ' . $response->getBody());
         }
 
         $result = $response->getBody()->__toString();
